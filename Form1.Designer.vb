767a9e6fef8414aca97143961c5bbeaab64f0b07
Yunochii0 <thepencerry04@gmail.com>
2024-10-08 12:59:42 +0800

    Add files via upload
    BC3MD

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        BindingSource1 = New BindingSource(components)
+        ListBox1 = New ListBox()
+        Label1 = New Label()
+        CType(BindingSource1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(12, 109)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(179, 27)
+        Button1.TabIndex = 0
+        Button1.Text = "Add Item"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(214, 109)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(127, 27)
+        Button2.TabIndex = 1
+        Button2.Text = "Remove"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 152)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(329, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(12, 194)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(329, 244)
+        ListBox1.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Arial Rounded MT Bold", 45F, FontStyle.Underline)
+        Label1.Location = New Point(15, 17)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(320, 70)
+        Label1.TabIndex = 4
+        Label1.Text = "To Do List"
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.PowderBlue
+        ClientSize = New Size(353, 450)
+        Controls.Add(Label1)
+        Controls.Add(ListBox1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "To Do List -BC3MD"
+        CType(BindingSource1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ListBox1.Items.Add(TextBox1.Text)
+        TextBox1.Select()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If ListBox1.SelectedIndex >= 0 Then
+            ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+        Else
+            MessageBox.Show("Please select an item to remove.")
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
